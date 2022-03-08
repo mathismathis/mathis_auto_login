f33647d013876dea3d4ad0b30bf473e2e8b1a3b1
mathis <mathismathis2000tt@gmail.com>
2022-03-08 19:36:44 +0530

    adding cor functionnality

--- src/example_package/mathis.py
+++ src/example_package/mathis.py
@@ -1,6 +1,16 @@
 import webbrowser
 
 
-url='https://www.youtube.com/watch?v=v4bkJef4W94'
+url='https://www.instagram.com/maestro_mathis/?hl=en'
+url1='https://www.linkedin.com/in/mathis-t-794254222/'
+url2='https://www.youtube.com/channel/UChAqVuW3nJCWdbbJOYEP00Q'
+url3='https://www.youtube.com/channel/UChAqVuW3nJCWdbbJOYEP00Q'
 
-webbrowser.open(url)
+def instagram():
+    webbrowser.open(url)
+def linkedin():
+    webbrowser.open(url1)
+def youtube():
+    webbrowser.open(url2)
+def facebook():
+    webbrowser.open(url3)
